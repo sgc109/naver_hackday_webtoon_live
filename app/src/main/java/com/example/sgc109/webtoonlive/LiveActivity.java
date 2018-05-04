c79971baa755b756c20682fcb8d28083ecfc4547
sgc109 <sgc109109@gmail.com>
2018-05-04 21:24:57 +0900

    asdf

--- app/src/main/java/com/example/sgc109/webtoonlive/LiveActivity.java
+++ app/src/main/java/com/example/sgc109/webtoonlive/LiveActivity.java
@@ -2,11 +2,15 @@ package com.example.sgc109.webtoonlive;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PointF;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.LinearSmoothScroller;
 import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -72,7 +76,7 @@ public class LiveActivity extends AppCompatActivity {
             }
         };
 
-        LinearLayoutManager layoutManager;
+        final LinearLayoutManager layoutManager;
         if(mIsWriter) {
             layoutManager = new LinearLayoutManager(this);
 
@@ -81,20 +85,20 @@ public class LiveActivity extends AppCompatActivity {
                 public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
                     super.onScrolled(recyclerView, dx, dy);
                     mCurPosition += dy;
-                    Log.d("scroll_debug", "onScrolled Start");
-                    Log.d("scroll_debug", "last : " + mLastCheckedTime);
-                    Log.d("scroll_debug", "now  : " + new Date().getTime());
-
+//                    Log.d("scroll_debug", "onScrolled Start");
+//                    Log.d("scroll_debug", "last : " + mLastCheckedTime);
+//                    Log.d("scroll_debug", "now  : " + new Date().getTime());
+                    Log.d("scroll_debug", "curY : " + mCurPosition);
                     Long now = new Date().getTime();
                     if(now - mLastCheckedTime >= 500) {
-                        Log.d("scroll_debug", "FirebaseDB Push!!");
+//                        Log.d("scroll_debug", "FirebaseDB Push!!");
                         DatabaseReference ref = mDatabase.child(getString(R.string.firebase_db_pos_history));
                         ref
                                 .push()
                                 .setValue(new VerticalPositionChanged(mCurPosition));
                         mLastCheckedTime = now;
                     }
-                    Log.d("scroll_debug", "onScrolled Finish");
+//                    Log.d("scroll_debug", "onScrolled Finish");
                 }
             };
             mRecyclerView.addOnScrollListener(scrollListener);
@@ -106,9 +110,10 @@ public class LiveActivity extends AppCompatActivity {
             ref.addChildEventListener(new ChildEventListener() {
                 @Override
                 public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                    Log.d("mydebug", "onChildAdded");
-
-
+                    int pos = dataSnapshot.getValue(VerticalPositionChanged.class).position;
+                    Log.d("mydebug", "pos : " + pos);
+//                    mRecyclerView.smoothScrollToPosition(pos);
+                    ((LinearLayoutManager)mRecyclerView.getLayoutManager()).scrollToPositionWithOffset(0, pos);
                 }
 
                 @Override
@@ -155,6 +160,34 @@ public class LiveActivity extends AppCompatActivity {
 
     public class ScrollDisablingLayoutManager extends LinearLayoutManager {
         private boolean isScrollEnabled = false;
+//        private static final float MILLISECONDS_PER_INCH = 175f; //default is 25f (bigger = slower)
+//        public ScrollDisablingLayoutManager(Context context, int orientation, boolean reverseLayout) {
+//            super(context, orientation, reverseLayout);
+//        }
+//
+//        public ScrollDisablingLayoutManager(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+//            super(context, attrs, defStyleAttr, defStyleRes);
+//        }
+//
+//        @Override
+//        public void smoothScrollToPosition(RecyclerView recyclerView, RecyclerView.State state, int position) {
+//
+//            final LinearSmoothScroller linearSmoothScroller = new LinearSmoothScroller(recyclerView.getContext()) {
+//
+//                @Override
+//                public PointF computeScrollVectorForPosition(int targetPosition) {
+//                    return super.computeScrollVectorForPosition(targetPosition);
+//                }
+//
+//                @Override
+//                protected float calculateSpeedPerPixel(DisplayMetrics displayMetrics) {
+//                    return MILLISECONDS_PER_INCH / displayMetrics.densityDpi;
+//                }
+//            };
+//
+//            linearSmoothScroller.setTargetPosition(position);
+//            startSmoothScroll(linearSmoothScroller);
+//        }
 
         public ScrollDisablingLayoutManager(Context context) {
             super(context);
--- app/src/main/java/com/example/sgc109/webtoonlive/VerticalPositionChanged.java
+++ app/src/main/java/com/example/sgc109/webtoonlive/VerticalPositionChanged.java
@@ -2,6 +2,7 @@ package com.example.sgc109.webtoonlive;
 
 public class VerticalPositionChanged {
     public int position;
+    public VerticalPositionChanged(){}
     public VerticalPositionChanged(int position){
         this.position = position;
     }
